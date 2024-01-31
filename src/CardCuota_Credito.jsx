5f3ba638c40f7d6ed95365998810a5072fd681d5
Nicolas Heredia <nicoherediasp@gmail.com>
2024-01-30 22:56:53 -0300

    fix menor: agregado simbolo "$" que faltaba

--- src/CardCuota_Credito.jsx
+++ src/CardCuota_Credito.jsx
@@ -6,11 +6,11 @@ function CardCuota_Credito (props) {
     const { cuota, montoFinal, porcentaje , monto} = props;
     return (
         <>
-        <div className="cardCuota fade-in lg:w-1/5 w-2/5 fade-in">
+        <div className="cardCuota fade-in lg:w-1/5 w-2/5 fade-in flex">
                     <div className="cardCuota__numero">{cuota}</div>
                     <div className="cardCuota__contenedorMontos">
-                        <p className="cardCuota__precioFinal">Precio Final {Math.round(monto * montoFinal).toLocaleString("de-DE")}</p>
-                        <p className="cardCuota__precioCuotas">{cuota} cuotas de {Math.round((monto * montoFinal) / cuota).toLocaleString("de-DE")}</p>
+                        <p className="cardCuota__precioFinal">Precio Final ${Math.round(monto * montoFinal).toLocaleString("de-DE")}</p>
+                        <p className="cardCuota__precioCuotas">{cuota} cuotas de ${Math.round((monto * montoFinal) / cuota).toLocaleString("de-DE")}</p>
                         <div className="cardCuota__porcentaje">{porcentaje}%</div>
                     </div>
                 </div>
--- src/CardCuota_Custom.jsx
+++ src/CardCuota_Custom.jsx
@@ -8,7 +8,7 @@ function CardCuota_Custom(props) {
         <div className="cardCuota fade-in lg:w-1/5 w-2/5 fade-in">
                     <div className="cardCuota__numero">{cuota}</div>
                     <div className="cardCuota__contenedorMontos">
-                        <p className="cardCuota__precioFinal">Precio Final {Math.round(monto * montoFinal).toLocaleString("de-DE")}</p>
+                        <p className="cardCuota__precioFinal">Precio Final ${Math.round(monto * montoFinal).toLocaleString("de-DE")}</p>
                         <p className="cardCuota__precioCuotas">{mensaje}</p>
                         <div className="cardCuota__porcentaje">{porcentaje}%</div>
                     </div>

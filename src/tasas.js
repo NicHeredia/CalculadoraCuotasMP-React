98ec09cd0c3c073db608f5e58db16bdd44e14271
nichered <nicoherediasp@gmail.com>
2024-12-22 02:47:09 -0300

    actualizacion tasas

--- src/tasas.js
+++ src/tasas.js
@@ -1,15 +1,15 @@
 export const tasa_cuota_simple = {
     cuotas1 : 1.115448,
     debito : 1.070662,
-    cuotas3 : 1.217282,
-    cuotas6 : 1.32626,
-    cuotas9 : 1.440924,
-    cuotas12 : 1.570844
+    cuotas3 : 1.193034,
+    cuotas6 : 1.277794,
+    cuotas9 : 1.368738,
+    cuotas12 : 1.466708
 }
 export const tasa_naranja = {
     naranja2 : 1.285514,
-    naranja3 : 1.353914,
-    naranja6 : 1.573068
+    naranja3 : 1.363914,
+    naranja6 : 1.583068
 }
 
 export const costos_compra = {
@@ -19,14 +19,14 @@ export const costos_compra = {
 
 export const porcentajes = {
     debito : 8,
-    cuotas1 : 12,
+    cuotas1 : 11,
     naranja2 : 29,
     naranja3 : 36,
     naranja6 : 58,
-    ahora3 : 22,
-    ahora6 : 33,
-    ahora9 : 44,
-    ahora12 : 58  
+    ahora3 : 19,
+    ahora6 : 27,
+    ahora9 : 36,
+    ahora12 : 46  
 }
 
 
